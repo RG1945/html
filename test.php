00b9b9127e3555e87dbd0434226e329425573294
root <gruet.richard@gmail.com>
2017-05-08 18:32:50 +0200

    added a comment

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceci est une page de test avec des balises PHP</title>
+        <!-- A web page consists of two parts: the header and the body -->
+       <meta charset="ansi" />
+    </head>
+    <body>
+       <?php
+	   // Ci-dessous, la fonction qui calcule le volume du cône
+	   function VolumeCone($rayon, $hauteur)
+	   {
+	      $volume = $rayon * $rayon * 3.14 * $hauteur * (1/3); // calcul du volume
+	      return $volume; // indique la valeur à renvoyer, ici le volume
+	   }
+
+	   $volume = VolumeCone(3, 1);
+	   echo 'Le volume d\'un cône de rayon 3 et de hauteur 1 est de ' . $volume;
+	?>
+    </body>
+</html>
